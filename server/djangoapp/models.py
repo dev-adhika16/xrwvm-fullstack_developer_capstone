f4c1015544f045caec7b53fbf27da69ed4fb431e
Adhika P. <246798493+dev-adhika16@users.noreply.github.com>
2025-12-05 20:34:51 +0700

    Refactor: Improve code formatting and structure in migration, model, view, and API files

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,7 +1,4 @@
-# Uncomment the following imports before adding the Model code
-
 from django.db import models
-from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
@@ -19,6 +16,7 @@ class CarMake(models.Model):
 
     def __str__(self):
         return self.name  # Return the name as the string representation
+
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many
@@ -38,12 +36,18 @@ class CarModel(models.Model):
         ('WAGON', 'Wagon'),
         # Add more choices as required
     ]
-    type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
-    year = models.IntegerField(default=2023,
+    type = models.CharField(
+        max_length=10,
+        choices=CAR_TYPES,
+        default='SUV'
+    )
+    year = models.IntegerField(
+        default=2023,
         validators=[
             MaxValueValidator(2023),
             MinValueValidator(2015)
-        ])
+        ]
+    )
     # Other fields as needed
 
     def __str__(self):
--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,4 +1,3 @@
-# Uncomment the imports before you add the code
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
@@ -14,11 +13,23 @@ urlpatterns = [
     path(route='registration', view=views.registration, name='registration'),
 
     # path for dealer reviews view
-    path(route='get_cars', view=views.get_cars, name ='getcars'),
+    path(route='get_cars', view=views.get_cars, name='getcars'),
     path(route='get_dealers/', view=views.get_dealerships, name='get_dealers'),
-    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
-    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
-    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_reviews'),
+    path(
+        route='get_dealers/<str:state>',
+        view=views.get_dealerships,
+        name='get_dealers_by_state'
+    ),
+    path(
+        route='dealer/<int:dealer_id>',
+        view=views.get_dealer_details,
+        name='dealer_details'
+    ),
+    path(
+        route='reviews/dealer/<int:dealer_id>',
+        view=views.get_dealer_reviews,
+        name='dealer_reviews'
+    ),
 
     # path for add a review view
     path(route='add_review', view=views.add_review, name='add_review'),
