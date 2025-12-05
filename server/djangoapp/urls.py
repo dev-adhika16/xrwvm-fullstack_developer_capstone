98b7a577c5985db312d1463cc8e0256483f6a730
Adhika P. <246798493+dev-adhika16@users.noreply.github.com>
2025-12-05 10:29:05 +0700

    Implement models

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -14,6 +14,7 @@ urlpatterns = [
     path(route='registration', view=views.registration, name='registration'),
 
     # path for dealer reviews view
+    path(route='get_cars', view=views.get_cars, name ='getcars'),
 
     # path for add a review view
 
